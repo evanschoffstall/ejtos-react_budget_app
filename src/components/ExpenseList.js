3eecb4e20e484f54d26366ed89f3e7e578701455
Evan Schoffstall <157645696+evanschoffstall@users.noreply.github.com>
2024-02-25 22:37:45 -0500

    new header color

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -8,7 +8,7 @@ const ExpenseList = () => {
   return (
     <table className="table">
       <thead className="thead-light">
-        <tr style={{ backgroundColor: "#969696", color: "#fff" }}>
+        <tr style={{ backgroundColor: "#212529", color: "#fff" }}>
           <th scope="col">Department</th>
           <th scope="col">Allocated Budget</th>
           <th scope="col" style={{ textAlign: "center" }}>
